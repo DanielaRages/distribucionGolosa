9d069c69d416558580028b4782025fec55cfdbee
leandro <leandro@Leandro>
2020-12-01 19:47:55 -0300

    Test de distancia

--- distribucionGolosa/src/Test/calculoDistanciaRectaTest.java
+++ distribucionGolosa/src/Test/calculoDistanciaRectaTest.java
@@ -1,7 +1,20 @@
 package Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 import Logica.calculoDistanciaRecta; 
 
 public class calculoDistanciaRectaTest {
+	
+	@Test
+	public void calcularDistancia() 
+	{
+		calculoDistanciaRecta distancia = new calculoDistanciaRecta();
+		double km = distancia.calcularSemiverseno(-54.807222, -68.304444, -53.783333,-67.7);
+		assertTrue(km == 120.41768294002374);
+	}
 
 }
